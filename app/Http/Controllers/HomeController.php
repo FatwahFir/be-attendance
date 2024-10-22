fa2229ef28a40ea090c6ebacc83743a9a0b00427
Fatwah Firmansyah <fatwah091@gmail.com>
2024-10-22 17:14:06 +0700

    fixing recap data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,21 +25,22 @@ class HomeController extends Controller
             $currentYear = Carbon::now()->year;
 
             $attendanceData = Attendance::select(
-                    DB::raw("MONTHNAME(created_at) as month"),
+                    DB::raw("MONTH(created_at) as month"),
                     DB::raw("COUNT(*) as count")
                 )
                 ->whereYear('created_at', $currentYear)
                 ->where('type', 'in')
-                ->groupBy(DB::raw("MONTHNAME(created_at)"))
+                ->groupBy(DB::raw("MONTH(created_at)"))
                 ->orderBy(DB::raw("MONTH(created_at)"))
                 ->get();
-
+            
             $attendanceByMonth = $attendanceData->map(function ($item) {
                 return [
                     'x' => $item->month,
                     'y' => $item->count
                 ];
             });
+        
 
             return response()->json([
                 'status' => 'success',
